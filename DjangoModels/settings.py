f32850da1f97c8abf02d3652cb55e00531c7920a
Ganesh Neupane <ganeshneupane1357@gmail.com>
2025-09-17 09:04:35 +0545

    updated vs

--- DjangoModels/settings.py
+++ DjangoModels/settings.py
@@ -79,8 +79,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 
-#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-#STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 
 
 ROOT_URLCONF = 'DjangoModels.urls'
