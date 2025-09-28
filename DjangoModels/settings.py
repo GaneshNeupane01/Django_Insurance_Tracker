26d0d780cfc5efa1dcf6741e8f8dbb2eba7da6a7
Ganesh Neupane <ganeshneupane1357@gmail.com>
2025-09-28 15:10:17 +0545

    updated with postgre

--- DjangoModels/settings.py
+++ DjangoModels/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 import os
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -117,13 +118,29 @@ WSGI_APPLICATION = 'DjangoModels.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
+# Default to SQLite (local dev)
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+
+
+
+
+
+
+
+
+# If DATABASE_URL is set (Render/Postgres), override with Postgres config
+DATABASE_URL = os.getenv("DATABASE_URL")
+if DATABASE_URL:
+    DATABASES["default"] = dj_database_url.config(default=DATABASE_URL, conn_max_age=600, ssl_require=True)
+
+
+
 
 
 # Password validation
