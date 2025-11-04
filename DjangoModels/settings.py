f26a5f54fbd33fced4553ff579580b0646d4cd3a
Ganesh Neupane <ganeshneupane1357@gmail.com>
2025-11-04 19:56:10 +0545

    fixed cloudinary

--- DjangoModels/settings.py
+++ DjangoModels/settings.py
@@ -104,9 +104,18 @@ CLOUDINARY_STORAGE = {
     'CLOUD_NAME': os.getenv('CLOUDINARY_CLOUD_NAME'),
     'API_KEY': os.getenv('CLOUDINARY_API_KEY'),
     'API_SECRET': os.getenv('CLOUDINARY_API_SECRET'),
+
+
 }
 
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+STORAGES = {
+    'default': {
+        'BACKEND': 'cloudinary_storage.storage.MediaCloudinaryStorage',
+    },
+    'staticfiles': {
+        'BACKEND': 'whitenoise.storage.CompressedManifestStaticFilesStorage',
+    },
+}
 
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
